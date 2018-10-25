96da0bd6e2b4d6cf5bd72eccc55d2bf7735a6f78
Vikas Jain <vikasjain03@gmail.com>
2018-10-25 18:12:57 +1100

    resolved dependency issues

--- au-gov-nsw-education-dc/core/src/main/java/au/gov/nsw/education/dc/core/models/HelloWorldModel.java
+++ au-gov-nsw-education-dc/core/src/main/java/au/gov/nsw/education/dc/core/models/HelloWorldModel.java
@@ -16,21 +16,18 @@
 package au.gov.nsw.education.dc.core.models;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.inject.Named;
 
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.settings.SlingSettingsService;
 
 @Model(adaptables=Resource.class)
 public class HelloWorldModel {
 
-    @Inject
+   
     private SlingSettingsService settings;
 
-    @Inject @Named("sling:resourceType") @Default(values="No resourceType")
+   
     protected String resourceType;
 
     private String message;
